44a45623f4c37e486f3e8a6f43f64ebcd9f5106b
binello7 <binello7@localhost>
2017-12-22 13:48:33 +0100

    clean up

--- inst/terrain.m
+++
@@ -1,4 +0,0 @@
-function c = terrain (N=64)
-[~, txt] = system (sprintf ("python3 -c \"import matplotlib.pyplot as plt;import numpy as np;x = np.linspace(0,1,%d);print (plt.get_cmap(\'terrain\')(x))\"",N));
-eval(sprintf ('c = %s(:,1:3);', txt(1:end-1)));
-endfunction
