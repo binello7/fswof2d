466bd0a17fcc0fa8eaf5b7d9f5697ab679cd13cc
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-04-06 12:56:55 +0200

    Make code Matlab-compatible

--- examples/channel-middleweir-varQ/s_ChannelMiddleWeir_input.m
+++ examples/channel-middleweir-varQ/s_ChannelMiddleWeir_input.m
@@ -1,61 +1,56 @@
-## Copyright (C) 2018 Sebastiano Rusca
-##
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##
-## Author: Sebastiano Rusca <sebastiano.rusca@gmail.com>
-## Created: 2018-01-02
+%% Copyright (C) 2018 Sebastiano Rusca
+%%
+%% This program is free software; you can redistribute it and/or modify it
+%% under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
+%% Author: Sebastiano Rusca <sebastiano.rusca@gmail.com>
+%% Created: 2018-01-02
 
 pkg load fswof2d
 pkg load linear-algebra
 
-## Global parameters
-#
+%% Global parameters
 dataFolder  = 'data';
-studyName   = 'ChannelMiddleWeir';
 
-## Study variable
-# These are the variables that we will change in this study
+%% Study variable
+% Variables to change in the study
 nQ        = 25;
 Qin       = linspace (-0.2, -5, nQ);
-# Order to explore extremes
+
+% Order to explore extremes
 tmp = zeros (1,nQ);
 tmp(1:2:end) = Qin(1:ceil(end/2));
 tmp(2:2:end) = fliplr (Qin)(1:ceil(end/2)-1);
 Qin = tmp;
 clear tmp
 
-## Generate the topography
-#  First the cross-section shape, which will be a "rectangle" 2.5 m wide
-
+%% Generate the topography
+%  First the cross-section shape, which will be a "rectangle" 2.5 m wide
 B = 2.5;
 Nx = 50;
 x = linspace (0, B, Nx+1);
 xc = node2center (x);
 z = zeros (1, Nx);
 z(1) = z(end) = 2;
-#x = linspace (0, 2*R, Nx+1);
-#z = R - sqrt (R^2 - (x-R).^2);
 
-#  Extrude the cross-section along the y-axis
+%  Extrude the cross-section along the y-axis
 Ly = 40;
 Ny = 80;
 y = linspace (0, Ly, Ny+1);
 yc = node2center (y);
 [XX YY ZZc] = extrude_csec (xc, yc, z);
 
-
-#  Rotate the cross-section by the chosen slope value alpha
+%  Rotate the cross-section by the chosen slope value alpha
 alpha = -10;
 rx_alpha = rotv ([1 0 0], deg2rad (alpha));
 np = rx_alpha * [0 0 1].';
@@ -63,14 +58,14 @@ ZZp = -(np(1)*XX + np(2)*YY) / np(3);
 ZZ = ZZp + ZZc;
 
 
-## Generate initial conditions for h, u and v can be set to 0
+%% Generate initial conditions for h, u and v can be set to 0
 HH = ones (Ny, Nx);
 UU = zeros (Ny, Nx);
 VV = zeros (Ny, Nx);
 HH(:,1) = HH(:,end) = 0;
 
 
-#  Generate weir at some point of the channel
+%  Generate weir at some point of the channel
 wh  = 1;
 pos = 20;
 ZZ(pos,2:end-1) += wh;
@@ -81,11 +76,11 @@ HZ(pos+1:end,2:end-1) = ZZ(pos+1:end,2:end-1) + HH(pos+1:end,2:end-1);
 surf (XX, YY, ZZ);
 hold on
 mesh (XX, YY, HZ, 'facecolor', 'none', 'edgecolor', 'b');
-#axis equal
+%axis equal
 hold off
 
-## Generate needed inputs files and folders for FullSWOF_2D
-#  Generate Folders
+%% Generate needed inputs files and folders for FullSWOF_2D
+%  Generate Folders
 studyFolder   = fullfile (dataFolder, studyName);
 inputsFolder  = fullfile (studyFolder, 'Inputs');
 outputsFolder = fullfile (studyFolder, 'Outputs');
@@ -95,22 +90,22 @@ endif
 
 fname         = @(s) fullfile (inputsFolder, s);
 
-#  Convert the data to the FullSWOF_2D format
+%  Convert the data to the FullSWOF_2D format
 [X Y Z H U V] = dataconvert ('fswof2d', XX, YY, ZZ, HH, UU, VV);
 
-#  Write the topography to the file
+%  Write the topography to the file
 topo2file (X, Y, Z, fname ('topography.dat'));
 
-#  Write the initial conditions to the file
+%  Write the initial conditions to the file
 huv2file (X, Y, H, U, V, fname ('huv_init.dat'));
 
-# Save the experiment discharge values
+% Save the experiment discharge values
 save (fname ('Qin_values.dat'), 'Qin');
 
-# Save variables for reuse in reading outputs
+% Save variables for reuse in reading outputs
 save (fname ('variables.dat'), studyFolder, inputsFolder, outputsFolder);
 
-#  Write the simulation parameters to a file
+%  Write the simulation parameters to a file
 sim_duration    = 100;
 saved_timesteps = 100;
 top_boundary    = 5;
@@ -127,12 +122,12 @@ ns = floor (log10 (nQ)) + 1;
 suffix_fmt = sprintf ("_%%0%dd", ns);
 
 for i=1:nQ
-  # update Q
+  % update Q
   p.TopImposedQ = Qin(i);
-  # update output suffix
+  % update output suffix
   suffix               = sprintf (suffix_fmt, i);
   p.OutputsSuffix = suffix;
-  # update filename
+  % update filename
   pfile             = fname (sprintf ("parameters%s.txt", suffix));
   p.ParamsFile = pfile;
 
@@ -145,13 +140,13 @@ for i=1:nQ
   endif
 endfor
 
-## Write bash script to run study
+%% Write bash script to run study
 bfile = fullfile (studyFolder, "run.sh");
 printf ("Writing Bash script to file %s\n", bfile); fflush (stdout);
 timetxt = strftime ("%Y-%m-%d %H:%M:%S", localtime (time ()));
 bsh = {
-'#!/bin/bash', ...
-sprintf("## Automatically generated on %s\n", timetxt), ...
+'%!/bin/bash', ...
+sprintf("%% Automatically generated on %s\n", timetxt), ...
 sprintf("for i in {1..%d}; do", nQ), ...
 sprintf("  id=`printf %s $i`", suffix_fmt), ...
 '  nohup fswof2d -f parameters$id.txt &', ...
@@ -163,4 +158,4 @@ fid = fopen (bfile, "wt");
 fputs (fid, bsh);
 fclose (fid);
 
-# save variables.dat X Y Z Nx Ny sim_duration studyFolder inputsFolder outputsFolder
+% save variables.dat X Y Z Nx Ny sim_duration studyFolder inputsFolder outputsFolder
