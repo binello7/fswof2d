6ac383d34fcbe5267eb2fa977c509156e40b3a30
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-04-06 17:29:50 +0200

    Remove saving frames

--- examples/channel-middleweir-varQ/s_ChannelMiddleWeir_output.m
+++ examples/channel-middleweir-varQ/s_ChannelMiddleWeir_output.m
@@ -17,25 +17,22 @@
 %% Created: 2017-12-11
 
 pkg load fswof2d
-close all
 
 if ~exist ('HZ_evl', 'var')
+
   %% Global parameters
-  %
   dataFolder  = "data";
-  studyName   = "ChannelMiddleWeir";
-  suffix      = 2;
+  exp_nb      = 2;
   fsuf = @(s, n) strcat (s, sprintf ('_%02d', n));
 
   %% Read outputs from files
-  %
-  outputsFolder = fullfile (dataFolder, studyName, fsuf ('Outputs', suffix));
+  outputsFolder = fullfile (dataFolder, fsuf ('Outputs', exp_nb));
   fname         = @(s) fullfile (outputsFolder, s);
 
   data_init  = load (fname ('huz_initial.dat'));
   data_evl  = load (fname ('huz_evolution.dat'));
 
-  paramsfile = fullfile (dataFolder, studyName, 'Inputs', strcat (fsuf ('parameters', suffix), '.txt'));
+  paramsfile = fullfile (dataFolder, 'Inputs', strcat (fsuf ('parameters', exp_nb), '.txt'));
   params = read_params (paramsfile);
 
   %% Get topography and final state free surface
@@ -59,57 +56,33 @@ if ~exist ('HZ_evl', 'var')
     fr_evl(:,t)     = data_evl(span,9);
     HZ_evl(:,:,t)   = dataconvert ('octave', [Nx Ny], hz_evl(:,t));
     Fr_evl(:,:,t)   = dataconvert ('octave', [Nx Ny], fr_evl(:,t));
-  endfor
-endif
+  end%for
+end%if
 
 az = 70;
 el = 45;
 
 t_str = @(t) sprintf ("t = %d s", t);
-figure(1,"position",get(0,"screensize"))
 ZZ = HZ_evl - Z;
 ZZ(ZZ==0) = NA;
 ZZ = ZZ + Z;
+
+%% Play simulation
+figure (1,"position",get(0,"screensize"))
 view (az, el);
 g3 = surf (X, Y, ZZ(:,:,1), 'edgecolor', 'none');
 shading interp
 colormap(ocean(64));
-%colormap jet
-colorbar
-t1 = text (2, -20, 0, t_str (1));
+t1 = text (10, -20, 20, t_str (1), 'fontsize', 15);
 hold on
 mesh (X, Y, Z, 'facecolor', 'w','edgecolor','k');
 hold off
-%axis ([min(X(:)) max(X(:)) min(Y(:)) max(Y(:)) min(Z(:)) max(Z(:))])
-%axis square
 axis equal
 grid on;
 
-sv = false;
-% Save first frame if 'sv' is active
-if sv
-  framesFolder = fullfile (dataFolder, studyName, 'frames');
-  mkdir (framesFolder);
-  frame = fullfile (framesFolder, sprintf ('frame-%04d.png', 1));
-  print ('-dpng', '-r300', '-S1920,1080', frame);
-endif
 
 for t = 2:tsteps
   set (g3, 'zdata', ZZ(:,:,t));
   set (t1, 'string', t_str (t));
-  pause(0.5);
-  if sv
-    %saves each frame if 'sv' is active
-    frame = fullfile (framesFolder, sprintf ('frame-%04d.png', t));
-    print ('-dpng', '-r300', '-S1920,1080', frame);
-  endif
-endfor
-
-if sv
-  close (3)
-  system ('ffmpeg -f image2 -i data/Channel_ConstQ/frames/frame-%04d.png -vcodec mpeg4 data/Channel_ConstQ/animation.mp4');
-  bidon=input ('Frames created ; press <enter> to start assembling');
-  disp ('Video assembled, folder ''frames'' can be erased!')
-endif
-
-h_crest = mean (HZ_evl(1,:,end) - Z(1,:));
+  pause(0.2);
+end%for
